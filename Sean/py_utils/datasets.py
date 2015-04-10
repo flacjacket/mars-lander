a33ab61044fbaa9ec97437a53f4eb5dcaacd51c4
Sean Vig <sean.v.775@gmail.com>
2015-04-10 00:16:55 -0500

    Ready to train on full data?

--- Sean/py_utils/datasets.py
+++ Sean/py_utils/datasets.py
@@ -3,14 +3,11 @@ from __future__ import print_function, division
 import numpy as np
 import os
 
-from pylearn2.datasets import dense_design_matrix
 from py_utils.pgm import read_pgm
 from py_utils.raw import read_raw
 
-if os.name == 'nt':
-    jpl_dir = os.path.normpath(r'C:\Users\seanvig2\Desktop\Source\mars-lander')
-else:
-    jpl_dir = os.path.normpath(os.path.expanduser('~/mars-lander'))
+this_dir = os.path.split(os.path.abspath(__file__))[0]
+jpl_root = os.path.split(os.path.split(this_dir)[0])[0]
 
 difficulty = {
     1: 'S0C0R10',
@@ -19,11 +16,17 @@ difficulty = {
     4: 'S4C4R20'
 }
 
-train_dir = os.path.join(jpl_dir, 'training data')
+train_dir = os.path.join(jpl_root, 'training data')
 
 data_height = {
     n: os.path.join(
         train_dir, 'terrain{0}_100', 'terrain{0}_100_500by500_dem.raw'
+    ).format(d) for n, d in difficulty.items()
+}
+
+data_image = {
+    n: os.path.join(
+        train_dir, 'terrain{0}_100', 'terrain{0}_100.pgm'
     ).format(d) for n, d in difficulty.items()
 }
 
@@ -34,7 +37,7 @@ data_labels = {
 }
 
 R = 17
-BUFFER = 20
+BUFFER = 21
 
 INPUT_SHAPE = 1000
 OUTPUT_SHAPE = INPUT_SHAPE - 2 * BUFFER
@@ -43,46 +46,3 @@ RAW_SHAPE = 500
 RAW_BUFFER = BUFFER // 2
 
 N_FEAT = 1 + 2 * ((R + 1) // 2)
-
-
-class HeightDataset(dense_design_matrix.DenseDesignMatrix):
-    def __init__(self, which_set, hardness=1):
-
-        print("Using dataset:", difficulty[hardness])
-
-        h = read_raw(data_height[hardness], RAW_SHAPE, RAW_SHAPE)
-
-        i = 0
-        X = np.empty(((RAW_SHAPE-2*RAW_BUFFER)**2, N_FEAT**2), dtype=np.float32)
-        for xi in range(RAW_BUFFER, RAW_SHAPE-RAW_BUFFER):
-            for yi in range(RAW_BUFFER, RAW_SHAPE-RAW_BUFFER):
-                X[i, :] = \
-                    h[xi-(R+1)//2:xi+(R+3)//2, yi-(R+1)//2:yi+(R+3)//2].flatten()
-                i += 1
-
-        assert i == (RAW_SHAPE-2*RAW_BUFFER)**2
-
-        X = np.hstack([X, X, X, X])
-        X = X.reshape(OUTPUT_SHAPE**2, N_FEAT**2)
-
-        y = read_pgm(data_labels[hardness])
-        y = y[BUFFER:INPUT_SHAPE-BUFFER, BUFFER:INPUT_SHAPE-BUFFER].flatten()
-
-        assert X.shape[0] == y.shape[0]
-
-        n_train = int(OUTPUT_SHAPE**2 * 0.7)
-        if which_set == 'train':
-            print("Training data")
-            X = X[:n_train, :]
-            y = y[:n_train]
-        elif which_set == 'valid':
-            print("Validation data")
-            X = X[n_train:, :]
-            y = y[n_train:]
-        else:
-            raise ValueError("Invalid set:", which_set)
-
-        super(HeightDataset, self).__init__(
-            X=X,
-            y=y.flatten().reshape(-1, 1)
-        )
---
+++ Sean/train NN/make_dataset.py
@@ -0,0 +1,59 @@
+import numpy as np
+import os
+import sys
+
+from pylearn2.utils import serial
+
+this_dir = os.path.split(os.path.abspath(__file__))[0]
+jpl_dir = os.path.split(this_dir)[0]
+
+sys.path.append(jpl_dir)
+
+from py_utils.datasets import INPUT_SHAPE, BUFFER, R, data_image, data_labels
+from py_utils.pgm import read_pgm
+
+
+n_samples = (INPUT_SHAPE - 2 * BUFFER)**2
+n_train = (INPUT_SHAPE - 2 * BUFFER) * 400
+n_test = (INPUT_SHAPE - 2 * BUFFER) * 200
+
+n_features = (2 * R + 1)**2
+
+X = np.zeros((n_train, n_features), dtype=np.float16)
+X_test = np.zeros((n_test, n_features), dtype=np.float16)
+
+y = np.zeros((n_train, 1), dtype=int)
+y_test = np.zeros((n_test, 1), dtype=int)
+
+data = read_pgm(data_image[1])
+labels = read_pgm(data_image[1])
+
+norm_data = np.zeros_like(data, dtype=np.float16)
+norm_data[:] = data / 255
+
+
+def get_data(n):
+    i = n // (INPUT_SHAPE - 2 * BUFFER)
+    j = n % (INPUT_SHAPE - 2 * BUFFER)
+    return norm_data[BUFFER+i-R:BUFFER+i+R+1, BUFFER+j-R:BUFFER+j+R+1]
+
+
+def get_label(n):
+    i = n // (INPUT_SHAPE - 2 * BUFFER)
+    j = n % (INPUT_SHAPE - 2 * BUFFER)
+    return int(labels[BUFFER+i, BUFFER+j] == 0xff)
+
+
+for i in range(n_train):
+    X[i, :] = get_data(i).flatten()
+    y[i, 0] = get_label(i)
+
+for i in range(n_test):
+    X_test[i, :] = get_data(i + n_train).flatten()
+    y_test[i, 0] = get_label(i + n_train)
+
+
+serial.save("X_train.pkl", X)
+serial.save("X_test.pkl", X_test)
+serial.save('y_train.pkl', y)
+serial.save('y_test.pkl', y_test)
